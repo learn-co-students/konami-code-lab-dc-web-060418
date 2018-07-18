9a8b2f5ef8e73710b99bae0b61754e735ad10a34
orenmagid <autoimpedicis@gmail.com>
2018-07-18 07:53:03 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+
+  const key = e.key;
+
+  index = (codes[index] === key ) ? ++index : 0
+
+  if (index === codes.length) {
+    window.alert("Hurray!");
+    index = 0
+
+  }
+});
+
+
 }
