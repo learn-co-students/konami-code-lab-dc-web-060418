bd8723faeb1f5cacfef84257e160706992248816
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-17 20:04:01 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,26 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0;
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(e) {
+    console.log(e.key);
+    konamiListener(e);
+  })
 }
+
+function konamiListener(e) {
+
+  if (e.key === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      console.log("You've cracked the code!");
+      alert("You've cracked the code!");
+      index = 0;
+    }
+  };
+}
+
+
