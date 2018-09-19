27a6424339e3511c522200540018eb616e9e8321
j-shilling <shilling.jake@gmail.com>
2018-09-19 10:38:42 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,17 @@ const codes = [
   "a"
 ];
 
+let index = 0;
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(e) {
+    if (e.key === codes[index++]) {
+      if (index === codes.length) {
+        window.alert("YAYAYA");
+        index = 0;
+      }
+    } else {
+      console.log("Bad key: " + e.key);
+      index = 0;
+    }
+  });
 }
